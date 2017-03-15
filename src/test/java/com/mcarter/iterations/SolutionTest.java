8a7ad7d0cfaf55bbb89c7d5c7bacaef3245eafe8
Michael Carter <mcarter@gresham-computing.com>
2017-03-15 15:29:25 +0000

    Failing test - count zeros

--- src/test/java/com/mcarter/iterations/SolutionTest.java
+++ src/test/java/com/mcarter/iterations/SolutionTest.java
@@ -16,7 +16,7 @@ public class SolutionTest {
 
 	@Parameters(name = "{index}: when integer is {0} then max binary gap is {1}")
 	public static Iterable<Object[]> data() {
-		return Arrays.asList(new Object[][] { { 1041, 5 }, {529, 4}, {9, 2} });
+		return Arrays.asList(new Object[][] { { 1041, 5 }, {529, 4}, {9, 2}, {66561, 9} });
 	}
 
 	@Parameter(0) public int fInput;
