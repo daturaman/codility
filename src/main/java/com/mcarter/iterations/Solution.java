77c28221055ad946cc6668070a07eb82065282f5
Michael Carter <mcarter@gresham-computing.com>
2017-03-15 12:43:49 +0000

    Initial commit

---
+++ src/main/java/com/mcarter/iterations/Solution.java
@@ -0,0 +1,19 @@
+/*
+ * <copyright>
+ *
+ * Copyright (c) 2010-2017 Gresham Technologies plc. All rights reserved.
+ *
+ * </copyright>
+ */
+package com.mcarter.iterations;
+
+/**
+ * Return the largest binary gap - zeros surrounded by ones - in an integer.
+ *
+ * @author mcarter
+ */
+public class Solution {
+	public int solution(int N){
+		return 0;
+	}
+}
---
+++ src/test/java/com/mcarter/iterations/SolutionTest.java
@@ -0,0 +1,29 @@
+package com.mcarter.iterations;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class SolutionTest {
+
+	@Parameters(name = "{index}: when integer is {0} then max binary gap is {1}")
+	public static Iterable<Object[]> data() {
+		return Arrays.asList(new Object[][] { { 1041, 5 }, {529, 4}, {9, 2} });
+	}
+
+	@Parameter(0) public int fInput;
+	@Parameter(1) public int fExpected;
+
+	@Test
+	public void solution() throws Exception {
+		assertThat(new Solution().solution(fInput), is(fExpected));
+	}
+}
